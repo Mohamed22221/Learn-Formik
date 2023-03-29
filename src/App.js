92f279e63f6a7ea5e3d63bb833ea53ec2923c230
unknown <hamdymohame7@gmail.com>
2023-03-29 17:13:32 +0200

    done valodate formik

--- src/App.js
+++ src/App.js
@@ -3,42 +3,78 @@ import "./App.css";
 import { useFormik } from "formik";
 
 function App() {
-  const formik = useFormik({
-    initialValues: {
-      name: "",
-      email: "",
-      password: "",
-    },
-    onSubmit : (data) =>{
-      console.log(data)
+  const initialValues = {
+    name: "",
+    email: "",
+    password: "",
+  };
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  const validate = (values) => {
+    const errors = {};
+    if (!values.name) {
+      errors.name = "Required";
     }
-  });
+    if (!values.email) {
+      errors.email = "Required";
+    } else if (
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+    ) {
+      errors.email = "Invalid email address";
+    }
+    if (!values.password) {
+      errors.password = "Required";
+    }
 
-  console.log(formik);
+    return errors;
+  };
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    validate,
+  });
+  console.log(formik.errors, "formik");
+
   return (
     <div className="App">
       <form onSubmit={formik.handleSubmit}>
-        <label>Name</label>
-        <input
-          name="name"
-          type="text"
-          value={formik.values.name}
-          onChange={formik.handleChange}
-        />
-        <label>Email</label>
-        <input
-          name="email"
-          type="email"
-          value={formik.values.email}
-          onChange={formik.handleChange}
-        />
-        <label>Password</label>
-        <input
-          name="password"
-          type="password"
-          value={formik.values.password}
-          onChange={formik.handleChange}
-        />
+        <div className="form-control">
+          <label>Name</label>
+          <input
+            name="name"
+            type="text"
+            value={formik.values.name}
+            onChange={formik.handleChange}
+          />
+          {formik.errors.name ? (
+            <div className="error">{formik.errors.name}</div>
+          ) : null}
+        </div>
+        <div className="form-control">
+          <label>Email</label>
+          <input
+            name="email"
+            type="email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+          />
+          {formik.errors.email ? (
+            <div className="error">{formik.errors.email}</div>
+          ) : null}
+        </div>
+        <div className="form-control">
+          <label>Password</label>
+          <input
+            name="password"
+            type="password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+          />
+          {formik.errors.password ? (
+            <div className="error">{formik.errors.password}</div>
+          ) : null}
+        </div>
         <button type="submit">Send</button>
       </form>
     </div>
