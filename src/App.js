6180e61ee1b82a79b27b0e69ab06eea0f1f00e19
unknown <hamdymohame7@gmail.com>
2023-03-29 19:36:54 +0200

    done validation YUP formik

--- src/App.js
+++ src/App.js
@@ -1,40 +1,32 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useFormik } from "formik";
-
+import { useFormik, yupToFormErrors } from "formik";
+import { object, string  } from 'yup';
 function App() {
+  const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
   const initialValues = {
     name: "",
     email: "",
     password: "",
+    phoneNumber : ""
   };
   const onSubmit = (data) => {
     console.log(data);
   };
-  const validate = (values) => {
-    const errors = {};
-    if (!values.name) {
-      errors.name = "Required";
-    }
-    if (!values.email) {
-      errors.email = "Required";
-    } else if (
-      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-    ) {
-      errors.email = "Invalid email address";
-    }
-    if (!values.password) {
-      errors.password = "Required";
-    }
+  const validationSchema = object().shape({
+    name: string().required("required").min(2, 'Too Short!').max(50, "Too Long!"),
+    email:string().email("Invalid email").required("required"),
+    password: string().required("required"),
+    phoneNumber: string().matches(phoneRegExp, 'Phone number is not valid').required("required").optional()
+  })
 
-    return errors;
-  };
   const formik = useFormik({
     initialValues,
     onSubmit,
-    validate,
+    validationSchema,
+
   });
-  console.log(formik.touched, "formik touch");
+  console.log(formik.isValid, "formik touch");
 
   return (
     <div className="App">
@@ -66,6 +58,19 @@ function App() {
           ) : null}
         </div>
         <div className="form-control">
+          <label>phone</label>
+          <input
+            name="phoneNumber"
+            type="number"
+            value={formik.values.phoneNumber}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          {formik.touched.phoneNumber && formik.errors.phoneNumber ? (
+            <div className="error">{formik.errors.phoneNumber}</div>
+          ) : null}
+        </div>
+        <div className="form-control">
           <label>Password</label>
           <input
             name="password"
@@ -78,7 +83,7 @@ function App() {
             <div className="error">{formik.errors.password}</div>
           ) : null}
         </div>
-        <button type="submit">Send</button>
+        <button type="submit"   disabled={!(formik.dirty && formik.isValid)}>Send</button>
       </form>
     </div>
   );
