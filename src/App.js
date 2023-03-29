3e54d77928dded1f126ada73a8e8eab3632fd92a
unknown <hamdymohame7@gmail.com>
2023-03-29 15:50:58 +0200

    useFormik and submit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useFormik } from "formik";
 
 function App() {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+    onSubmit : (data) =>{
+      console.log(data)
+    }
+  });
+
+  console.log(formik);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={formik.handleSubmit}>
+        <label>Name</label>
+        <input
+          name="name"
+          type="text"
+          value={formik.values.name}
+          onChange={formik.handleChange}
+        />
+        <label>Email</label>
+        <input
+          name="email"
+          type="email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+        />
+        <label>Password</label>
+        <input
+          name="password"
+          type="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+        />
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
 }
